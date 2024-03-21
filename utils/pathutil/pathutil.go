8dd857f424d2ea3494753306bd3063859d84a361
samsaralc <962488477@qq.com>
2024-03-21 13:54:05 +0800

    feat: path

--- utils/pathutil/pathutil.go
+++ utils/pathutil/pathutil.go
@@ -1,6 +1,7 @@
 package pathutil
 
 import (
+	"strings"
 	"time"
 )
 
@@ -10,4 +11,19 @@ func GetCurrentDateOnlyPath(path string) string {
 		return format
 	}
 	return format + "/" + path
+}
+
+// GetMinioAccessPath path的第一个/前面是bucket
+// 将path的第一个/后面添加当前日期
+// 如果path中没有/，则直接在path后面添加当前日期
+
+func getMinioPath(path string) string {
+	trim := strings.Trim(path, "/")
+	index := strings.Index(trim, "/")
+	if index == -1 {
+		path = trim + "/" + GetCurrentDateOnlyPath("")
+	} else {
+		path = trim + "/" + GetCurrentDateOnlyPath(trim[index+1:])
+	}
+	return path
 }
