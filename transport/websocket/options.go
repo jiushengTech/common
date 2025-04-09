0a7cefa3530cebab6f27894b64dd411222b96bc7
liuchao <liuchao@qq.com>
2025-04-09 23:51:23 +0800

    feat: 去掉无用的代码

--- transport/websocket/options.go
+++ transport/websocket/options.go
@@ -1,100 +1,27 @@
 package websocket
 
-import (
-	"crypto/tls"
-	"net"
-	"time"
+import "github.com/gorilla/websocket"
 
-	"github.com/go-kratos/kratos/v2/encoding"
-)
+// Option 是 WebSocket 客户端选项类型
+type Option func(o *WebSocketServer)
 
-type PayloadType uint8
-
-const (
-	PayloadTypeBinary = 0
-	PayloadTypeText   = 1
-)
-
-type ServerOption func(o *Server)
-
-func WithNetwork(network string) ServerOption {
-	return func(s *Server) {
-		s.network = network
+// WithAddr 设置服务器地址
+func WithAddr(addr string) Option {
+	return func(s *WebSocketServer) {
+		s.addr = addr
 	}
 }
 
-func WithAddress(addr string) ServerOption {
-	return func(s *Server) {
-		s.address = addr
+// WithURL 设置 WebSocket 连接的 url
+func WithURL(u string) Option {
+	return func(c *WebSocketServer) {
+		c.url = u
 	}
 }
 
-func WithTimeout(timeout time.Duration) ServerOption {
-	return func(s *Server) {
-		s.timeout = timeout
-	}
-}
-
-func WithPath(path string) ServerOption {
-	return func(s *Server) {
-		s.path = path
-	}
-}
-
-func WithConnectHandle(h ConnectHandler) ServerOption {
-	return func(s *Server) {
-		s.sessionMgr.RegisterConnectHandler(h)
-	}
-}
-
-func WithTLSConfig(c *tls.Config) ServerOption {
-	return func(o *Server) {
-		o.tlsConf = c
-	}
-}
-
-func WithListener(lis net.Listener) ServerOption {
-	return func(s *Server) {
-		s.lis = lis
-	}
-}
-
-func WithCodec(c string) ServerOption {
-	return func(s *Server) {
-		s.codec = encoding.GetCodec(c)
-	}
-}
-
-func WithChannelBufferSize(size int) ServerOption {
-	return func(_ *Server) {
-		channelBufSize = size
-	}
-}
-
-func WithPayloadType(payloadType PayloadType) ServerOption {
-	return func(s *Server) {
-		s.payloadType = payloadType
-	}
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-type ClientOption func(o *Client)
-
-func WithClientCodec(c string) ClientOption {
-	return func(o *Client) {
-		o.codec = encoding.GetCodec(c)
-	}
-}
-
-func WithEndpoint(uri string) ClientOption {
-	return func(o *Client) {
-		o.url = uri
-	}
-}
-
-func WithClientPayloadType(payloadType PayloadType) ClientOption {
-	return func(c *Client) {
-		c.payloadType = payloadType
+// WithUpgrader  设置 WebSocket 连接的请求头
+func WithUpgrader(upgrader *websocket.Upgrader) Option {
+	return func(c *WebSocketServer) {
+		c.upgrader = upgrader
 	}
 }
