38f790ff266d88804e6826de4de808069e440881
samsaralc <962488477@qq.com>
2025-07-23 00:13:08 +0800

    refactor: parallelize log testing with goroutines

--- log/zap/logger/log_test.go
+++ log/zap/logger/log_test.go
@@ -1,26 +1,35 @@
 package logger
 
 import (
+	"sync"
 	"testing"
-	"time"
 )
 
 func TestLog(t *testing.T) {
-
-	for range 1000 {
-		time.Sleep(1 * time.Second)
-		Slog.Debug("test debug")
-		Slog.Debugf("test debugf %s", "debugf")
-		Slog.Debugw("test debugw", "debugw")
-		Slog.Info("test info")
-		Slog.Infof("test infof %s", "infof")
-		Slog.Infow("test infow", "infow")
-		Slog.Warn("test warn")
-		Slog.Warnf("test warnf %s", "warnf")
-		Slog.Warnw("test warnw", "warnw")
-		Slog.Error("test error")
-		Slog.Errorf("test errorf %s", "errorf")
-		Slog.Errorw("test errorw", "errorw")
-
+	r := func() {
+		for range 1000 {
+			Slog.Debug("test debug")
+			Slog.Debugf("test debugf %s", "debugf")
+			Slog.Debugw("test debugw", "debugw")
+			Slog.Info("test info")
+			Slog.Infof("test infof %s", "infof")
+			Slog.Infow("test infow", "infow")
+			Slog.Warn("test warn")
+			Slog.Warnf("test warnf %s", "warnf")
+			Slog.Warnw("test warnw", "warnw")
+			Slog.Error("test error")
+			Slog.Errorf("test errorf %s", "errorf")
+			Slog.Errorw("test errorw", "errorw")
+		}
 	}
+	group := sync.WaitGroup{}
+	group.Add(10)
+	for _ = range 10 {
+		go func() {
+			defer group.Done()
+			r()
+		}()
+	}
+	group.Wait()
+
 }
