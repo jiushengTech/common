c94deaa1675e3361391fa5b7ec57fee6d76d8fa2
samsaralc <962488477@qq.com>
2025-09-24 14:49:42 +0800

    feat: wrap sonyflake in Snowflake struct and add ID generation methods with retries

--- id/snowflake.go
+++ id/snowflake.go
@@ -1,11 +1,18 @@
 package id
 
 import (
+	"time"
+
 	"github.com/sony/sonyflake/v2"
 )
 
+// Snowflake 对 sonyflake.Sonyflake 进行面向对象封装
+type Snowflake struct {
+	flake *sonyflake.Sonyflake
+}
+
 // NewSnowflake 新建 Sonyflake 并应用传递的选项
-func NewSnowflake(opts ...Option) (*sonyflake.Sonyflake, error) {
+func NewSnowflake(opts ...Option) (*Snowflake, error) {
 	settings := sonyflake.Settings{}
 	// 应用所有传递的选项
 	for _, opt := range opts {
@@ -16,5 +23,29 @@ func NewSnowflake(opts ...Option) (*sonyflake.Sonyflake, error) {
 	if err != nil {
 		return nil, err
 	}
-	return s, nil
+	return &Snowflake{flake: s}, nil
+}
+
+// Uint64 获取一个 uint64 类型的唯一 ID，失败返回0
+func (s *Snowflake) Uint64() uint64 {
+	return uint64(s.Int64())
+}
+
+// Int64 获取一个 int64 类型的唯一 ID，内部重试3次，最终失败返回0
+func (s *Snowflake) Int64() int64 {
+	if s == nil || s.flake == nil {
+		// snowflake未初始化直接返回0
+		return 0
+	}
+	for i := 0; i < 3; i++ {
+		id64, e := s.flake.NextID()
+		if e == nil {
+			return id64
+		}
+		// 可加短暂延迟再重试
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	// 最终失败返回0
+	return 0
 }
